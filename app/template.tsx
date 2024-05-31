1633069cf4a56ce8d64687ff77493f52906bea56
hthanhlong <hthanhlong97@gmail.com>
2024-05-31 14:18:54 -0700

    build: template for transition

---
+++ app/template.tsx
@@ -0,0 +1,12 @@
+"use client"
+import { motion } from "framer-motion"
+
+const Template = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.5 }}>
+      {children}
+    </motion.div>
+  )
+}
+
+export default Template

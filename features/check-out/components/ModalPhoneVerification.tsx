3bc916eadb99a93fac3255ab6a6e21b6b5ea8f4a
hthanhlong <hthanhlong97@gmail.com>
2024-06-06 11:59:52 -0700

    build: modal phone verification cont

--- features/check-out/components/ModalPhoneVerification.tsx
+++ features/check-out/components/ModalPhoneVerification.tsx
@@ -5,19 +5,18 @@ import { NUMBER_OF_INPUTS } from "../data"
 
 const ModalPhoneVerification = () => {
   const [inputValues, setInputValues] = useState<string[]>(Array(NUMBER_OF_INPUTS).fill(""))
-  // const [activeIndex, setActiveIndex] = useState<number>(0)
+  const [focusIndex, setFocusIndex] = useState<number>(0)
 
   const handleOnchange = (e: React.ChangeEvent<HTMLInputElement>, index: number) => {
     const { value } = e.target
-    setInputValues((prev) => {
-      const newValues = [...prev]
-      newValues[index] = value
-      return newValues
-    })
     if (value) {
-      // if (index < NUMBER_OF_INPUTS - 1) {
-      //   setActiveIndex(index + 1)
-      // }
+      if (isNaN(Number(value))) return // only allow numbers
+      setInputValues((prev) => {
+        const updatedValues = [...prev]
+        updatedValues[index] = value
+        return updatedValues
+      })
+      setFocusIndex((prev) => (prev === NUMBER_OF_INPUTS - 1 ? prev : prev + 1))
     }
   }
 
@@ -38,6 +37,32 @@ const ModalPhoneVerification = () => {
     }
   }, [])
 
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === "Backspace") {
+        setInputValues((prev) => {
+          const updatedValues = [...prev]
+          if (updatedValues[focusIndex] !== "") {
+            updatedValues[focusIndex] = ""
+          }
+          return updatedValues
+        })
+        setFocusIndex((prev) => {
+          if (prev === 0) return prev
+          return prev - 1
+        })
+      }
+    }
+    window.addEventListener("keydown", handleKeyDown)
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown)
+    }
+  }, [inputValues, focusIndex])
+
+  useEffect(() => {
+    document.getElementById(`otp-${focusIndex}`)?.focus()
+  }, [focusIndex])
+
   return (
     <Modal show={true}>
       <Modal.Body>
@@ -49,13 +74,14 @@ const ModalPhoneVerification = () => {
               <div className="flex gap-4">
                 {Array.from({ length: NUMBER_OF_INPUTS }).map((_, index) => (
                   <input
+                    onClick={() => setFocusIndex(index)}
+                    id={`otp-${index}`}
                     key={index}
                     className="size-[50px] rounded-sm border border-gray-300 text-center"
                     type="text"
                     onChange={(e) => handleOnchange(e, index)}
                     maxLength={1}
                     value={inputValues[index]}
-                    // autoFocus={activeIndex === index}
                   />
                 ))}
               </div>

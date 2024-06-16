59813d05736470ad6501d3ccfff9e472b136991b
hthanhlong <hthanhlong97@gmail.com>
2024-06-15 22:45:59 -0700

    chore: remove redundant code

--- features/check-out/components/TotalPriceOnPlaceOrder.tsx
+++ features/check-out/components/TotalPriceOnPlaceOrder.tsx
@@ -1,11 +1,9 @@
 import clsx from "clsx"
 import React from "react"
-import { useIsCheckoutStore } from "stores/is-checkout-store"
 import useCheckout from "../hooks/useCheckout"
 
 const TotalPriceOnPlaceOrder = ({ finalPrice, status }: { finalPrice: string; status: string }) => {
   const { userId } = useCheckout()
-  const { setIsCheckout } = useIsCheckoutStore()
   return (
     <button
       disabled={!userId || status === "loading"}
@@ -14,7 +12,6 @@ const TotalPriceOnPlaceOrder = ({ finalPrice, status }: { finalPrice: string; st
         "flex-center mt-auto flex min-h-14 gap-5 bg-red-200 text-xl transition-all hover:bg-orange-400",
         !userId && "opacity-50 disabled:pointer-events-none"
       )}
-      onClick={() => setIsCheckout(true)}
     >
       {status === "loading" ? (
         "Loading..."
